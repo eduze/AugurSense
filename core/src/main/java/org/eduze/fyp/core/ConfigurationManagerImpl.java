8e5d0408dae84c38c2c1d382deaa1d0e5e3dc203
Imesha Sudasingha <imesha.sudasingha@gmail.com>
2018-04-11 13:09:12 +0530

    Restructured the modules. Only 2 available now, core and dist.

---
+++ core/src/main/java/org/eduze/fyp/core/ConfigurationManagerImpl.java
@@ -0,0 +1,272 @@
+/*
+ * Copyright 2017 Eduze
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to
+ * whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.eduze.fyp.core;
+
+import org.eduze.fyp.core.db.dao.CameraConfigDAO;
+import org.eduze.fyp.core.db.dao.ZoneDAO;
+import org.eduze.fyp.api.ConfigurationManager;
+import org.eduze.fyp.api.State;
+import org.eduze.fyp.api.StateManager;
+import org.eduze.fyp.api.annotations.AutoStart;
+import org.eduze.fyp.api.annotations.Mode;
+import org.eduze.fyp.api.listeners.ConfigurationListener;
+import org.eduze.fyp.api.model.CameraConfig;
+import org.eduze.fyp.api.model.CameraGroup;
+import org.eduze.fyp.api.model.PointMapping;
+import org.eduze.fyp.api.model.Zone;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.awt.image.ColorModel;
+import java.awt.image.WritableRaster;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.OptionalInt;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
+
+import static org.eduze.fyp.api.Constants.Properties.FLOOR_MAP_IMAGE;
+import static org.eduze.fyp.api.Constants.ZONE_NAME_WORLD;
+
+/**
+ * {@link ConfigurationManager} implementation which is doing all operations using in-memory data storage.
+ *
+ * @author Imesha Sudasingha
+ */
+@AutoStart(startOrder = 1, mode = Mode.PASSIVE)
+public class ConfigurationManagerImpl implements ConfigurationManager {
+
+    private static final Logger logger = LoggerFactory.getLogger(ConfigurationManagerImpl.class);
+
+    private final StateManager stateManager = new StateManager(State.STOPPED);
+
+    private String propertiesFile;
+    private BufferedImage map;
+
+    private Set<ConfigurationListener> configurationListeners = new HashSet<>();
+    private Map<Integer, CameraConfig> cameraConfigs = new ConcurrentHashMap<>();
+    private Map<Integer, CameraGroup> cameraGroups = new ConcurrentHashMap<>();
+    // TODO: 1/5/18 If zones are deleted through UI, it is not reflected here
+
+    private List<Zone> zones = new ArrayList<>();
+    private CameraConfigDAO cameraConfigDAO;
+    private ZoneDAO zoneDAO;
+
+    public ConfigurationManagerImpl() { }
+
+    @Override
+    public void start() {
+        stateManager.checkState(State.STOPPED);
+
+        if (propertiesFile == null) {
+            throw new IllegalArgumentException("No properties file is given");
+        }
+
+        setup();
+        try {
+            loadConfiguration();
+        } catch (IOException e) {
+            logger.error("Error occurred when loading configuration", e);
+            throw new IllegalArgumentException("Unable to load properties", e);
+        }
+
+        String mapPath = System.getProperty(FLOOR_MAP_IMAGE);
+        if (mapPath == null) {
+            throw new IllegalArgumentException("No Map File Path given");
+        }
+        logger.debug("Using map image : {}", mapPath);
+
+        try (InputStream in = getClass().getClassLoader().getResourceAsStream(mapPath)) {
+            map = ImageIO.read(in);
+        } catch (IOException e) {
+            logger.error("Unable to load the map image : {}", mapPath, e);
+            throw new IllegalArgumentException("Unable to load the map image");
+        }
+
+        stateManager.setState(State.STARTED);
+        notifyConfigurationChange();
+    }
+
+    private void setup() {
+        cameraConfigDAO.cameraGroups().forEach(cameraGroup -> {
+            List<Zone> zones = cameraGroup.getZones();
+            if (zones.stream().noneMatch(zone -> ZONE_NAME_WORLD.equals(zone.getZoneName()))) {
+                Zone world = new Zone(ZONE_NAME_WORLD, Integer.MAX_VALUE, null, null, cameraGroup);
+                logger.debug("Adding zone: [{}] to camera group: [{}]", world, cameraGroup);
+                zoneDAO.save(world);
+            }
+        });
+    }
+
+    private void loadConfiguration() throws IOException {
+        logger.info("Loading configurations");
+
+        try (InputStream propertiesFile = new FileInputStream(this.propertiesFile)) {
+            System.getProperties().load(propertiesFile);
+        }
+
+        this.zones.clear();
+        this.zones.addAll(zoneDAO.list());
+
+        this.cameraConfigDAO.list()
+                .forEach(cameraConfig -> this.cameraConfigs.put(cameraConfig.getCameraId(), cameraConfig));
+
+        this.cameraConfigDAO.cameraGroups().forEach(cameraGroup -> cameraGroups.put(cameraGroup.getId(), cameraGroup));
+    }
+
+    @Override
+    public synchronized void addCameraConfig(CameraConfig cameraConfig) {
+        stateManager.checkState(State.STARTED);
+        logger.debug("Adding camera config - {}. Total: {}", cameraConfig, cameraConfigs.size());
+        cameraConfigs.put(cameraConfig.getCameraId(), cameraConfig);
+        notifyConfigurationChange();
+    }
+
+    @Override
+    public synchronized void addPointMapping(int cameraId, PointMapping mappings) {
+        stateManager.checkState(State.STARTED);
+        cameraConfigs.get(cameraId).setPointMapping(mappings);
+        notifyConfigurationChange();
+    }
+
+    /**
+     * Returns the next camera ID to be taken by a camera
+     *
+     * @return next camera ID
+     */
+    @Override
+    public synchronized int getNextCameraId() {
+        stateManager.checkState(State.STARTED);
+
+        OptionalInt max = cameraConfigs.keySet().stream().mapToInt(Integer::intValue).max();
+        int nextInt = 1;
+        if (max.isPresent()) {
+            nextInt = max.getAsInt() + 1;
+        }
+
+        return nextInt;
+    }
+
+    @Override
+    public CameraConfig getCameraConfig(int cameraId) {
+        return cameraConfigs.get(cameraId).clone();
+    }
+
+    @Override
+    public Map<Integer, CameraConfig> getCameraConfigs() {
+        return cameraConfigs;
+    }
+
+    @Override
+    public BufferedImage getMap() {
+        stateManager.checkState(State.STARTED);
+        ColorModel cm = map.getColorModel();
+        boolean isAlphaPreMultiplied = cm.isAlphaPremultiplied();
+        WritableRaster raster = map.copyData(null);
+        return new BufferedImage(cm, raster, isAlphaPreMultiplied, null);
+    }
+
+    @Override
+    public List<Zone> getZones() {
+        return zones;
+    }
+
+    @Override
+    public PointMapping getPointMapping(int cameraId) {
+        return cameraConfigs.get(cameraId).getPointMapping();
+    }
+
+    @Override
+    public Set<Integer> getCameraIds() {
+        return cameraConfigs.keySet();
+    }
+
+    @Override
+    public Set<InetSocketAddress> getCameraIpAndPorts() {
+        return cameraConfigs.values().stream()
+                .map(config -> {
+                    String[] parts = config.getIpAndPort().split(":");
+                    return InetSocketAddress.createUnresolved(parts[0], Integer.parseInt(parts[1]));
+                }).collect(Collectors.toSet());
+    }
+
+    @Override
+    public int getNumberOfCameras() {
+        return cameraConfigs.entrySet().size();
+    }
+
+    @Override
+    public synchronized void addConfigurationListener(ConfigurationListener listener) {
+        configurationListeners.add(listener);
+    }
+
+    @Override
+    public synchronized void removeConfigurationListener(ConfigurationListener listener) {
+        configurationListeners.remove(listener);
+    }
+
+    @Override
+    public boolean isConfigured() {
+        return cameraConfigs.values().size() > 0 && cameraConfigs.values().stream()
+                .noneMatch(cameraConfig -> cameraConfig.getCameraGroup() == null);
+    }
+
+    private void notifyConfigurationChange() {
+        logger.debug("Notifying configuration change");
+        configurationListeners.forEach(listener -> listener.configurationChanged(this));
+    }
+
+    @Override
+    public void stop() {
+        stateManager.checkState(State.STARTED);
+        logger.info("Stopping Configuration Manager ...");
+        stateManager.setState(State.STOPPED);
+    }
+
+    public void setPropertiesFile(String propertiesFile) {
+        this.propertiesFile = propertiesFile;
+    }
+
+    public void setConfigurationListeners(Set<ConfigurationListener> configurationListeners) {
+        this.configurationListeners = configurationListeners;
+    }
+
+    public void setZoneDAO(ZoneDAO zoneDAO) {
+        this.zoneDAO = zoneDAO;
+    }
+
+    public void setCameraConfigDAO(CameraConfigDAO cameraConfigDAO) {
+        this.cameraConfigDAO = cameraConfigDAO;
+    }
+
+    public Map<Integer, CameraGroup> getCameraGroups() {
+        return cameraGroups;
+    }
+}
---
+++ core/src/main/java/org/eduze/fyp/core/MapProcessorImpl.java
@@ -0,0 +1,206 @@
+/*
+ * Copyright 2017 Eduze
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this
+ * software and associated documentation files (the "Software"), to deal in the Software
+ * without restriction, including without limitation the rights to use, copy, modify, merge,
+ * publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to
+ * whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package org.eduze.fyp.core;
+
+import org.eduze.fyp.core.resources.GlobalMap;
+import org.eduze.fyp.api.CameraCoordinator;
+import org.eduze.fyp.api.ConfigurationManager;
+import org.eduze.fyp.api.MapProcessor;
+import org.eduze.fyp.api.State;
+import org.eduze.fyp.api.StateManager;
+import org.eduze.fyp.api.annotations.AutoStart;
+import org.eduze.fyp.api.listeners.ProcessedMapListener;
+import org.eduze.fyp.api.model.CameraConfig;
+import org.eduze.fyp.api.model.CameraGroup;
+import org.eduze.fyp.api.resources.LocalMap;
+import org.eduze.fyp.api.resources.PersonSnapshot;
+import org.eduze.fyp.api.util.Args;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import static org.eduze.fyp.api.Constants.MAP_REFRESH_INTERVAL;
+import static org.eduze.fyp.api.Constants.MAP_REFRESH_THRESHOLD;
+
+/**
+ * Class responsible for processing local maps sent by multiple cameras and aggregating for global maps.
+ * TODO address configuration changes
+ *
+ * @author Imesha Sudasinha
+ * @author Madhawa Vidanapathirana
+ */
+@AutoStart(startOrder = 1)
+public class MapProcessorImpl implements MapProcessor {
+
+    private static final Logger logger = LoggerFactory.getLogger(MapProcessorImpl.class);
+
+    private final Map<Integer, GlobalMap> globalMaps = new HashMap<>();
+    private final StateManager stateManager = new StateManager(State.STOPPED);
+    private final Queue<LocalMap> localMapQueue = new LinkedList<>();
+    private CameraCoordinator cameraCoordinator;
+    private Set<ProcessedMapListener> mapListeners = new HashSet<>();
+    private ExecutorService processor;
+
+    private ConfigurationManager configurationManager;
+
+    private int zonePersistentThreshold;
+    private int zonePersistentScanCount;
+
+    @Override
+    public void addLocalMap(LocalMap map) {
+        stateManager.checkState(State.STARTED);
+        logger.debug("Adding a local map for camera : {}", map.getCameraId());
+
+        // Point mapping conversion
+        //        CameraConfig cameraConfig = configurationManager.getCameraConfig(map.getCameraId());
+        //        map.getPersonCoordinates().forEach(personCoordinate -> {
+        //            personCoordinate.setX(personCoordinate.getX() * Constants.CAMERA_VIEW_WIDTH / cameraConfig.getWidth());
+        //            personCoordinate.setY(personCoordinate.getY() * Constants.CAMERA_VIEW_HEIGHT / cameraConfig.getHeight());
+        //        });
+
+        synchronized (this) {
+            localMapQueue.add(map);
+        }
+    }
+
+    @Override
+    public synchronized void addProcessedMapListener(ProcessedMapListener listener) {
+        mapListeners.add(listener);
+    }
+
+    @Override
+    public void removeProcessedMapListener(ProcessedMapListener listener) {
+        mapListeners.remove(listener);
+    }
+
+    @Override
+    public void start() {
+        Args.notNull(cameraCoordinator, "cameraCoordinator");
+
+        configurationManager.getCameraGroups()
+                .forEach((id, cameraGroup) -> {
+                    GlobalMap globalMap = new GlobalMap();
+
+                    ZoneMapper zoneMapper = new ZoneMapper(cameraGroup.getZones(),
+                            zonePersistentScanCount, zonePersistentThreshold);
+                    globalMap.setZoneMapper(zoneMapper);
+                    //                    globalMap.setPhotoMapper(photoMapper);
+                    //                    globalMap.setAccuracyTester(accuracyTester);
+                    globalMaps.put(id, globalMap);
+                });
+
+        stateManager.checkState(State.STOPPED);
+        logger.debug("Starting map collector");
+
+        processor = Executors.newSingleThreadExecutor();
+        processor.submit((Runnable) () -> {
+            try {
+                stateManager.waitFor(State.STARTED);
+            } catch (InterruptedException e) {
+                logger.error("Map processing thread interrupted while waiting for start", e);
+            }
+            logger.debug("Starting map processing");
+
+            // TODO: 9/3/17 Optimize to use a thread pool of 2 at least to focus more on processing
+            long lastTimestamp = 0;
+            for (; ; ) {
+                if (!stateManager.isState(State.STARTED)) {
+                    break;
+                }
+
+                LocalMap nextMap;
+                synchronized (this) {
+                    nextMap = localMapQueue.poll();
+                }
+
+                try {
+                    GlobalMap globalMap = null;
+                    if (nextMap != null) {
+                        CameraConfig cameraConfig = configurationManager.getCameraConfig(nextMap.getCameraId());
+                        CameraGroup cameraGroup = cameraConfig.getCameraGroup();
+                        globalMap = globalMaps.get(cameraGroup.getId());
+                        globalMap.update(nextMap);
+
+                        List<List<PersonSnapshot>> snapshots = globalMap.getSnapshot();
+                        synchronized (this) {
+                            mapListeners.forEach(listener -> listener.mapProcessed(cameraGroup, snapshots));
+                        }
+                    }
+
+                    if (cameraCoordinator.getRealTimestamp() - lastTimestamp > MAP_REFRESH_INTERVAL) {
+                        lastTimestamp = cameraCoordinator.getRealTimestamp();
+                        long minTimestamp = lastTimestamp - MAP_REFRESH_THRESHOLD;
+                        globalMaps.values().forEach(map -> map.refresh(minTimestamp));
+                    }
+                } catch (Exception e) {
+                    logger.error("Error occurred in map processing", e);
+                }
+            }
+        });
+
+
+        stateManager.setState(State.STARTED);
+        logger.info("Map collector started");
+    }
+
+    @Override
+    public void stop() {
+        stateManager.checkState(State.STARTED);
+        logger.debug("Stopping map collector");
+
+        processor.shutdownNow();
+
+        logger.info("Map collector stopped");
+        stateManager.setState(State.STOPPED);
+    }
+
+    public synchronized void setMapListeners(Set<ProcessedMapListener> listeners) {
+        if (listeners != null) {
+            mapListeners.addAll(listeners);
+        }
+    }
+
+    public void setCameraCoordinator(CameraCoordinator cameraCoordinator) {
+        this.cameraCoordinator = cameraCoordinator;
+    }
+
+    public void setConfigurationManager(ConfigurationManager configurationManager) {
+        this.configurationManager = configurationManager;
+    }
+
+    public void setZonePersistentThreshold(int zonePersistentThreshold) {this.zonePersistentThreshold = zonePersistentThreshold;}
+
+    public int getZonePersistentThreshold() { return zonePersistentThreshold; }
+
+    public void setZonePersistentScanCount(int zonePersistentScanCount) {this.zonePersistentScanCount = zonePersistentScanCount;}
+
+    public int getZonePersistentScanCount() { return zonePersistentScanCount; }
+}
